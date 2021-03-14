ca993078cc3d39e1374489f013b594c9b32f34fb
TerenyaDanil <47912455+TerenyaDanil@users.noreply.github.com>
2021-03-14 19:35:31 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -48,3 +48,62 @@ function clickHandler(e) {
 		behavior: "smooth"
 	});
 }
+
+
+const squareRange = document.querySelector('.calc__range--square');
+const thicknessInput = document.querySelector('.calc__input--thickness');
+const calculationOutput = document.querySelector('.calc__result-output');
+
+let square = squareRange.value;
+let thickness = thicknessInput.value;
+let finalPrice = 0;
+
+const data = {
+	price50: 11000,
+	price5190: 220,
+	price91150: 200,
+	price151: 180,
+	cementBagPrice: 300,
+	sandZIL: 4000,
+	sandKAMAZ: 6000,
+	deviceDelivery: 3000,
+	cementDelivry: 1000,
+}
+
+squareRange.addEventListener('input', () => {
+	square = squareRange.value;
+	calculate()
+});
+thicknessInput.addEventListener('input', () => {
+	thickness = thicknessInput.value;
+	calculate()
+});
+
+function outputUpdate(value) {
+	document.querySelector('.calc__text--square').textContent = value;
+	calculate();
+}
+
+function calculate() {
+	if (square && thickness) {
+		finalPrice = 0;
+
+		let workPrice = 0;
+		if (square > 150) {
+			workPrice = square * data.price151;
+		} else if (square > 90) {
+			workPrice = square * data.price91150;
+		} else if (square > 50) {
+			workPrice = square * data.price5190;
+		} else {
+			workPrice = data.price50;
+		}
+
+		const cementBags = Math.ceil(square * 0.24);
+		const cementPrice = cementBags * data.cementBagPrice;
+		const sandPrice = (square * thickness / 100) > 4.7 ? 6000 : 4000;
+
+		finalPrice = workPrice + cementPrice + sandPrice + data.deviceDelivery + data.cementDelivry;
+		calculationOutput.textContent = finalPrice.toString() + '₽';
+	}
+}
